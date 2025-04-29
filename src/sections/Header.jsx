09aca769315462f9fdf7d55ab97d7deff95e7001
Legaz <worldhaha@mail.com>
2025-04-30 04:39:59 +0900

    Features section Ui

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,17 +1,43 @@
 import clsx from "clsx";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link as LinkScroll } from "react-scroll";
 
 const Header = () => {
+  const [hassScrolled, setHasScrolled] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
 
+  useEffect(() => {
+    const handleScroll = () => {
+      setHasScrolled(window.scrollY > 32);
+    };
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   const Navlink = ({ title }) => (
-    <LinkScroll className="base-bold text-p4 uppercase transition-colors duration-500 cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h5">
+    <LinkScroll
+      onClick={()=> setIsOpen(false)}
+      to={title}
+      offset={-100}
+      spy
+      smooth
+      activeClass="nav-active"
+      className="base-bold text-p4 uppercase transition-colors duration-500 cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h5"
+    >
       {title}
     </LinkScroll>
   );
+
   return (
-    <header className="fixed top-0 left-0 z-50 w-full py-10">
+    <header
+      className={clsx(
+        "fixed top-0 left-0 z-50 w-full py-10 transition-all duration-500 max-lg:py-4",
+        hassScrolled && "py-2 bg-black-100 backdrop-blur-[8px]"
+      )}
+    >
       <div className="container flex h-14 items-center max-lg:px-5">
         <a className="lg:hidden flex-1 cursor-pointer z-2">
           <img
@@ -40,12 +66,12 @@ const Header = () => {
                 </li>
                 <li className="nav-logo">
                   <LinkScroll
-                    to="Hero"
-                    offset={-100}
+                    to="hero"
+                    offset={-250}
                     spy
                     smooth
                     className={clsx(
-                      "max-lg:hidden tranisiton-transform duration-500 cursor-pinter"
+                      "max-lg:hidden tranisiton-transform duration-500 cursor-pointer"
                     )}
                   >
                     <img
@@ -63,8 +89,10 @@ const Header = () => {
                 </li>
               </ul>
             </nav>
-            <div className="lg:hidden block absolute top-1/2 left-0 w-[960px] 
-            h-[380px] translate-x-[-290px] -translate-y-1/2 rotate-90">
+            <div
+              className="lg:hidden block absolute top-1/2 left-0 w-[960px] 
+            h-[380px] translate-x-[-290px] -translate-y-1/2 rotate-90"
+            >
               <img
                 src="/public/images/bg-outlines.svg"
                 width={960}
