4f9ec92b2658db27f01036853b35b538e68384ba
Legaz <worldhaha@mail.com>
2025-04-30 02:27:30 +0900

    Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from './Header'
+import Hero from './Hero'
 
 const App = () => {
   return (
     <main className='overflow-hidden'>
-      <Header/>  
+      <Header/> 
+      <Hero /> 
     </main>
   )
 }
