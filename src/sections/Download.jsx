32d798ed7ebebfb086e6a8c16ce1e7878960d6e1
Legaz <worldoflegaz@icloud.com>
2025-05-01 16:57:43 +0900

    Testimonails UI section

---
+++ src/sections/Download.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Element } from 'react-scroll'
+
+const Download = () => {
+  return (
+    <section>
+        <Element name='download' className='g7 relative pb-32 pt-24 max-lg:pb'>
+
+        </Element>
+    </section>
+  )
+}
+
+export default Download
